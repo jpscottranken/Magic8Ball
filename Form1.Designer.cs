8d760447ae49e5dd401562b114392a9d3b3517fb
Jeff Scott <jpscott@ranken.edu>
2020-03-30 10:48:49 -0500

    first and only Magic 8 Ball commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Magic8Ball
+{
+    partial class FormMagic8Ball
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMagic8Ball));
+            this.buttonSubmitQuestion = new System.Windows.Forms.Button();
+            this.buttonResetForm = new System.Windows.Forms.Button();
+            this.buttonExitApplication = new System.Windows.Forms.Button();
+            this.textBoxAskQusestion = new System.Windows.Forms.TextBox();
+            this.pictureBoxMagic8Ball = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.submitQuestionMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetFormMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMagic8Ball)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSubmitQuestion
+            // 
+            this.buttonSubmitQuestion.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonSubmitQuestion.Location = new System.Drawing.Point(111, 449);
+            this.buttonSubmitQuestion.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonSubmitQuestion.Name = "buttonSubmitQuestion";
+            this.buttonSubmitQuestion.Size = new System.Drawing.Size(205, 108);
+            this.buttonSubmitQuestion.TabIndex = 1;
+            this.buttonSubmitQuestion.Text = "Submit Question";
+            this.buttonSubmitQuestion.UseVisualStyleBackColor = true;
+            this.buttonSubmitQuestion.Click += new System.EventHandler(this.buttonSubmitQuestion_Click);
+            // 
+            // buttonResetForm
+            // 
+            this.buttonResetForm.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonResetForm.Location = new System.Drawing.Point(441, 449);
+            this.buttonResetForm.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonResetForm.Name = "buttonResetForm";
+            this.buttonResetForm.Size = new System.Drawing.Size(205, 108);
+            this.buttonResetForm.TabIndex = 2;
+            this.buttonResetForm.Text = "Reset Form";
+            this.buttonResetForm.UseVisualStyleBackColor = true;
+            this.buttonResetForm.Click += new System.EventHandler(this.buttonResetForm_Click);
+            // 
+            // buttonExitApplication
+            // 
+            this.buttonExitApplication.Location = new System.Drawing.Point(784, 449);
+            this.buttonExitApplication.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonExitApplication.Name = "buttonExitApplication";
+            this.buttonExitApplication.Size = new System.Drawing.Size(205, 108);
+            this.buttonExitApplication.TabIndex = 3;
+            this.buttonExitApplication.Text = "Exit Application";
+            this.buttonExitApplication.UseVisualStyleBackColor = true;
+            this.buttonExitApplication.Click += new System.EventHandler(this.buttonExitApplication_Click);
+            // 
+            // textBoxAskQusestion
+            // 
+            this.textBoxAskQusestion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.textBoxAskQusestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAskQusestion.Location = new System.Drawing.Point(117, 275);
+            this.textBoxAskQusestion.Margin = new System.Windows.Forms.Padding(5);
+            this.textBoxAskQusestion.Multiline = true;
+            this.textBoxAskQusestion.Name = "textBoxAskQusestion";
+            this.textBoxAskQusestion.Size = new System.Drawing.Size(876, 99);
+            this.textBoxAskQusestion.TabIndex = 0;
+            // 
+            // pictureBoxMagic8Ball
+            // 
+            this.pictureBoxMagic8Ball.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxMagic8Ball.Image")));
+            this.pictureBoxMagic8Ball.Location = new System.Drawing.Point(427, 41);
+            this.pictureBoxMagic8Ball.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBoxMagic8Ball.Name = "pictureBoxMagic8Ball";
+            this.pictureBoxMagic8Ball.Size = new System.Drawing.Size(249, 203);
+            this.pictureBoxMagic8Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxMagic8Ball.TabIndex = 4;
+            this.pictureBoxMagic8Ball.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.submitQuestionMenuItem,
+            this.resetFormMenuItem,
+            this.exitMenuItem,
+            this.helpMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(10, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1125, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // submitQuestionMenuItem
+            // 
+            this.submitQuestionMenuItem.Name = "submitQuestionMenuItem";
+            this.submitQuestionMenuItem.Size = new System.Drawing.Size(108, 19);
+            this.submitQuestionMenuItem.Text = "Submit Question";
+            this.submitQuestionMenuItem.Click += new System.EventHandler(this.submitQuestionMenuItem_Click);
+            // 
+            // resetFormMenuItem
+            // 
+            this.resetFormMenuItem.Name = "resetFormMenuItem";
+            this.resetFormMenuItem.Size = new System.Drawing.Size(78, 19);
+            this.resetFormMenuItem.Text = "Reset Form";
+            this.resetFormMenuItem.Click += new System.EventHandler(this.resetFormMenuItem_Click);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(37, 19);
+            this.exitMenuItem.Text = "Exit";
+            this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+            // 
+            // helpMenuItem
+            // 
+            this.helpMenuItem.Name = "helpMenuItem";
+            this.helpMenuItem.Size = new System.Drawing.Size(44, 19);
+            this.helpMenuItem.Text = "Help";
+            this.helpMenuItem.Click += new System.EventHandler(this.helpMenuItem_Click);
+            // 
+            // FormMagic8Ball
+            // 
+            this.AcceptButton = this.buttonSubmitQuestion;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.CancelButton = this.buttonResetForm;
+            this.ClientSize = new System.Drawing.Size(1125, 918);
+            this.Controls.Add(this.pictureBoxMagic8Ball);
+            this.Controls.Add(this.textBoxAskQusestion);
+            this.Controls.Add(this.buttonExitApplication);
+            this.Controls.Add(this.buttonResetForm);
+            this.Controls.Add(this.buttonSubmitQuestion);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "FormMagic8Ball";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Magic 8 Ball - Sees All Knows All Tells All";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMagic8Ball)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSubmitQuestion;
+        private System.Windows.Forms.Button buttonResetForm;
+        private System.Windows.Forms.Button buttonExitApplication;
+        private System.Windows.Forms.TextBox textBoxAskQusestion;
+        private System.Windows.Forms.PictureBox pictureBoxMagic8Ball;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem submitQuestionMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetFormMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+//  Added for Sound Player
+using System.Media;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic8Ball
+{
+    public partial class FormMagic8Ball : Form
+    {
+        public FormMagic8Ball()
+        {
+            InitializeComponent();
+        }
+
+        //*************************************************
+        //
+        //  This method is called when the user
+        //  clicks the Submit Question button to
+        //  submit the question to the Magic 8 Ball.
+        //
+        //*************************************************
+        private void buttonSubmitQuestion_Click(object sender, EventArgs e)
+        {
+            SubmitQuestion();
+        }
+
+        //*************************************************
+        //
+        //  This method will submit the question to
+        //  the Magic 8 Ball, but it first checks to
+        //  see whether or not there was a question
+        //  actually inputted.
+        //
+        //  If no question is inputted, a Message is
+        //  given to the user and the method returns
+        //  with no further action required.
+        //
+        //  If anything was entere into the associated
+        //  textbox, we generate the answer, after
+        //  first, resetting the associated textbox
+        //  and button attributes.
+        //
+        //  This method is called by either the
+        //  Submit Question menu item or the Submit 
+        //  Question button.
+        //
+        //*************************************************
+        private void SubmitQuestion()
+        {
+            //  Flag for no input
+            bool noInput = false;   //  Assume there is input
+
+            //  Call routine to actually verify
+            //  that the user inputted something
+            //  into the textbox.
+            noInput = CheckForNoInput();
+
+            //  If textbox was empty, no further
+            //  action required.  Just return
+            //  from the method.
+            if (noInput)
+            {
+                return;
+            }
+
+            //  There was something inputted into
+            //  the textbox.  So, make the textbox
+            //  invisible, disable the Submit Question
+            //  button, and enable the Reset Form
+            //  button.
+
+            //  Make textbox invisible
+            textBoxAskQusestion.Visible = false;
+
+            //  Disable submit question button
+            buttonSubmitQuestion.Enabled = false;
+
+            //  Enable reset question button
+            buttonResetForm.Enabled = true;
+
+            //  Generate the associated answer
+            GenerateAnswer();
+        }
+
+        //*************************************************
+        //
+        //  This method will make the actual check
+        //  to verify that something was inputted
+        //  in the associated textbox.
+        //
+        //*************************************************
+        private bool CheckForNoInput()
+        {
+            //  Check for empty textbox input.
+            //
+            //  If no input, provide a message
+            //  for the user and return true
+            //  for the noInput flag.
+            //
+            //  If there was input, just
+            //  return true for the noInput
+            //  flag.
+            if (textBoxAskQusestion.Text == "")
+            {
+                MessageBox.Show("You Must Enter A Question!",
+                                "NO QUESTION ENTERED!",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation);
+                textBoxAskQusestion.Focus();
+                return true;
+            }
+
+            return false;
+        }
+
+        //*************************************************
+        //
+        //  This method will generate the answer
+        //  for the Magic 8 Ball.  It will also
+        //  change the associated 8 Ball image
+        //  and provide the "ta da" sound when
+        //  the image is changed.
+        //
+        //*************************************************
+        private void GenerateAnswer()
+        {
+            //  String which holds image # plus .png
+            string imageFileName = "";
+
+            //  Seed the rand
+            Random rand = new Random();
+
+            //  Generate random number between 1 and 15
+            int num = rand.Next(1, 16);
+
+            //  Set imageFileName to num + .png
+            imageFileName = num.ToString() + ".png";
+
+            //  Change the image on the screen
+            pictureBoxMagic8Ball.Image = System.Drawing.Image.FromFile(
+                @"C:\Users\Ranken\Desktop\Magic8Ball\Images\" + imageFileName);
+
+            //  Set the sizing of the image accordingly
+            pictureBoxMagic8Ball.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            //  Play the "ta da" sound along with this
+            var soundPlayer = new SoundPlayer(@"C:\Users\Ranken\Desktop\Magic8Ball\Sounds\tada.wav");
+            soundPlayer.Play();
+        }
+
+        //*************************************************
+        //
+        //  This method is called when the user
+        //  clicks the Reset Form button to
+        //  Reset the form.
+        //
+        //*************************************************
+        private void buttonResetForm_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        //*************************************************
+        //
+        //  This method will reset the form to its
+        //  original values.  It is called by either
+        //  the Reset Form menu item or the Reset 
+        //  Form button.
+        //
+        //*************************************************
+        private void ResetForm()
+        { 
+            //  Make textbox visible
+            textBoxAskQusestion.Visible = true;
+
+            //  Enable submit question button
+            buttonSubmitQuestion.Enabled = true;
+
+            //  Disable reset question button
+            buttonResetForm.Enabled = false;
+
+            //  Blank out textbox
+            textBoxAskQusestion.Text = "";
+
+            //  Reset image back to original Magic 8 Ball image
+            pictureBoxMagic8Ball.Image = System.Drawing.Image.FromFile(
+                @"C:\Users\Ranken\Desktop\Magic8Ball\Images\0.png");
+
+            //  Set the focus to the textbox
+            textBoxAskQusestion.Focus();
+        }
+
+        //*************************************************
+        //
+        //  This method is called when the user
+        //  clicks the Exit Application button to
+        //  possibly end the program run.
+        //
+        //*************************************************
+        private void buttonExitApplication_Click(object sender, EventArgs e)
+        {
+            ExitApplication();
+        }
+
+        //*************************************************
+        //
+        //  This method will allow the person running
+        //  the program to end the program run if s/he
+        //  so desires.  It is called by either the
+        //  Exit Application menu item or the Exit
+        //  Application button.
+        //
+        //*************************************************
+        private void ExitApplication()
+        {
+            //  Prompt user to see if s/he wants to end program now
+            if (MessageBox.Show("Do You Really Want To Exit The Program?",
+                            "EXIT PROGRAM NOW?!?!",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                //  Normal program termination
+                Application.Exit();
+            }
+        }
+
+        //*************************************************
+        //
+        //  This method is called when the user
+        //  clicks the Submit Question menu item to
+        //  submit the question to the Magic 8 Ball.
+        //
+        //*************************************************
+        private void submitQuestionMenuItem_Click(object sender, EventArgs e)
+        {
+            SubmitQuestion();
+        }
+
+        //*************************************************
+        //
+        //  This method is called when the user
+        //  clicks the Reset Form menu item to
+        //  Reset the form.
+        //
+        //*************************************************
+        private void resetFormMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        //*************************************************
+        //
+        //  This method is called when the user
+        //  clicks the Exit Application menu item
+        //  to possibly end the program run.
+        //
+        //*************************************************
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            ExitApplication();
+        }
+
+        private void helpMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowHelpMessage();
+        }
+
+        private void ShowHelpMessage()
+        {
+            string message  = "This is a Magic 8 Ball application.\n\n";
+                   message += "Key in a question in the purple textbox below.\n";
+                   message += "Then click the Submit Question button or Menu Option.\n";
+                   message += "Your answer will appear in the Magic 8 Ball.\n\n";
+                   message += "Click the Reset Form button or Menu Option to reset.\n\n";
+                   message += "Click the Exit button or Menu Option to quit.\n\n";
+                   message += "Like the 'real' Magic 8 Ball, this is for fun only!";
+
+            MessageBox.Show(message,
+                            "MAGIC 8 BALL INSTRUCTIONS",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+    }
+}
